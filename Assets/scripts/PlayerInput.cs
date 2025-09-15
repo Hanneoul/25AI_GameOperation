30059e8ae7a76b059c96a23e84d354c9930d0316
elflee77 <elflee77@gmail.com>
2025-09-15 16:56:17 +0900

    .

--- Assets/scripts/PlayerInput.cs
+++ Assets/scripts/PlayerInput.cs
@@ -7,19 +7,30 @@ public class PlayerInput : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     Rigidbody2D rb;
+    bool isJumping = false;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isJumping = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame && (isJumping == false) )
         {
+            isJumping = true;
             rb.linearVelocity = Vector2.up * JumpPower;
         }
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
     }
 }
